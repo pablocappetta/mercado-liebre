9414501070988a4ced5896217a8a0a6da3f665b1
Pablo <75391203+pablocappetta@users.noreply.github.com>
2021-09-21 20:13:24 -0300

    fixed responsive UI for tablets, laptops and laptops large

--- app.js
+++ app.js
@@ -11,4 +11,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.post("/registro", (req, res) => {
+  res.redirect("/");
+});
+
 app.use(express.static("public"));
